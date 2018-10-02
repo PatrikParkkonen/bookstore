e81aeabf6c4ae6d69ee41054abc070932df78604
SephKoulu <patrik.parkkonen@gmail.com>
2018-10-02 18:18:41 +0300

    First commit

---
+++ src/main/java/fi/haagahelia/project/Bookstore/web/BookstoreController.java
@@ -0,0 +1,22 @@
+package fi.haagahelia.project.Bookstore.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+@Controller
+@ResponseBody
+public class BookstoreController {
+	
+	@RequestMapping("/index")
+	public String indexHi() {
+		return "Welcome to the bookstore!";
+	}
+}
+
+
+
