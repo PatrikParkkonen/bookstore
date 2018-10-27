5413cc93fd6502addde8b8635112222d6f684536
SephKoulu <patrik.parkkonen@gmail.com>
2018-10-27 13:26:00 +0300

    Added REST

--- src/main/java/fi/haagahelia/project/Bookstore/domain/Book.java
+++ src/main/java/fi/haagahelia/project/Bookstore/domain/Book.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import fi.haagahelia.project.Bookstore.domain.Category;
 
@@ -23,6 +24,7 @@ public class Book {
 	private String price;
 	
 	 	@ManyToOne
+	 	@JsonIgnore
 	    @JoinColumn(name = "categoryid")
 	    private Category category;
 
--- src/main/java/fi/haagahelia/project/Bookstore/web/BookstoreController.java
+++ src/main/java/fi/haagahelia/project/Bookstore/web/BookstoreController.java
@@ -1,12 +1,16 @@
 package fi.haagahelia.project.Bookstore.web;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.project.Bookstore.domain.Book;
 import fi.haagahelia.project.Bookstore.domain.BookRepository;
@@ -31,6 +35,20 @@ public class BookstoreController {
         model.addAttribute("books", brepository.findAll());
         return "booklist";
     }
+    
+   
+        
+        @RequestMapping(value="/books", method = RequestMethod.GET)
+        public @ResponseBody List<Book> bookListRest() {
+        	return (List<Book>) brepository.findAll();
+    }
+        
+        @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+        public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+        	return brepository.findById(bookId);
+        }
+        
+        
     @RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
